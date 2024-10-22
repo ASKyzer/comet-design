3284587c1d0aca62b50e0415f28e223460febb36
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-22 03:22:04 +0200

    update comet.js

--- comet.js
+++ comet.js
@@ -1,5 +1,6 @@
 import "./src/theme/comet-styles.ts";
 
+// Comet components
 import "./src/components/accordion/comet-accordion.ts";
 import "./src/components/badge/comet-badge.ts";
 import "./src/components/banner/comet-banner.ts";
@@ -7,6 +8,8 @@ import "./src/components/button/comet-button.ts";
 import "./src/components/card/comet-card.ts";
 import "./src/components/color-picker/comet-color-picker.ts";
 import "./src/components/comet-default-button.ts";
+import "./src/components/comet-multi-select.ts";
+import "./src/components/comet-select.ts";
 import "./src/components/features-banner/comet-features-banner.ts";
 import "./src/components/feedback-message/comet-feedback-message.ts";
 import "./src/components/icon/comet-icon.ts";
@@ -14,3 +17,16 @@ import "./src/components/loading-spinner/comet-loading-spinner.ts";
 import "./src/components/options/comet-options.ts";
 import "./src/components/small-card/comet-small-card.ts";
 import "./src/components/toggle/comet-toggle.ts";
+
+// Vaadin components
+import "@vaadin/button";
+import "@vaadin/checkbox";
+import "@vaadin/checkbox-group";
+import "@vaadin/combo-box";
+import "@vaadin/horizontal-layout";
+import "@vaadin/multi-select-combo-box";
+import "@vaadin/notification";
+import "@vaadin/radio-group";
+import "@vaadin/select";
+import "@vaadin/tabs";
+import "@vaadin/vertical-layout";
