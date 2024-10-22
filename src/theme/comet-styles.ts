74445fa4d2b4ed45878f6d24496f20a1ca34616d
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-22 03:14:29 +0200

    add vaadin multi select combo box

--- src/theme/comet-styles.ts
+++ src/theme/comet-styles.ts
@@ -2,6 +2,7 @@ import "./vaadin/button";
 import "./vaadin/checkbox";
 import "./vaadin/checkbox-group";
 import "./vaadin/combo-box";
+import "./vaadin/multi-select-combo-box";
 import "./vaadin/notification";
 import "./vaadin/radio-button";
 import "./vaadin/tabs";
