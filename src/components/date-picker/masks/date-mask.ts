1d64d4b1a401dc486e7152aad9af419a7d7a8235
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-22 03:46:56 +0200

    create date picker

---
+++ src/components/date-picker/masks/date-mask.ts
@@ -0,0 +1,35 @@
+//based on https://maskito.dev/recipes/placeholder
+
+import type { MaskitoOptions } from "@maskito/core";
+import {
+  maskitoDateOptionsGenerator,
+  maskitoWithPlaceholder,
+} from "@maskito/kit";
+
+export const PLACEHOLDER = "dd.mm.yyyy";
+
+const dateOptions = maskitoDateOptionsGenerator({
+  mode: "dd/mm/yyyy",
+  separator: ".",
+});
+
+const {
+  plugins, // plugins keeps caret inside actual value and remove placeholder on blur
+  ...placeholderOptions
+  // pass 'true' as second argument to add plugin to hide placeholder when input is not focused
+} = maskitoWithPlaceholder(PLACEHOLDER, true);
+
+export default {
+  ...dateOptions,
+  plugins: plugins.concat(dateOptions.plugins || []),
+  preprocessors: [
+    // Always put it BEFORE all other preprocessors
+    ...placeholderOptions.preprocessors,
+    ...dateOptions.preprocessors,
+  ],
+  postprocessors: [
+    ...dateOptions.postprocessors,
+    // Always put it AFTER all other postprocessors
+    ...placeholderOptions.postprocessors,
+  ],
+} as Required<MaskitoOptions>;
