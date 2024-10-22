dde092e7eadb9fde39ab2fb6cfde372ad786f8d8
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-22 02:59:37 +0200

    create options component

--- comet.js
+++ comet.js
@@ -1,8 +1,16 @@
 import "./src/theme/comet-styles.ts";
 
 import "./src/components/accordion/comet-accordion.ts";
+import "./src/components/badge/comet-badge.ts";
+import "./src/components/banner/comet-banner.ts";
 import "./src/components/button/comet-button.ts";
 import "./src/components/card/comet-card.ts";
+import "./src/components/color-picker/comet-color-picker.ts";
+import "./src/components/comet-default-button.ts";
+import "./src/components/features-banner/comet-features-banner.ts";
+import "./src/components/feedback-message/comet-feedback-message.ts";
 import "./src/components/icon/comet-icon.ts";
+import "./src/components/loading-spinner/comet-loading-spinner.ts";
+import "./src/components/options/comet-options.ts";
 import "./src/components/small-card/comet-small-card.ts";
 import "./src/components/toggle/comet-toggle.ts";
