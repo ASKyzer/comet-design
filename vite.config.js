adf316cf5344933b24c464174a3b6cabaed53066
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-23 09:06:54 +0200

    add imports

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import fs from "fs";
@@ -31,7 +30,6 @@ export default defineConfig({
   assetsInclude: ["**/*.svg"],
   plugins: [
     resolve(),
-    commonjs(),
     typescript(),
     terser(),
     {
