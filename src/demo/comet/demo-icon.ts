21a8700595543d347b32938b0c5002733f1390a9
Adrian Kyzer <adriankyzer@gmail.com>
2024-10-28 11:36:23 +0100

    refactor icons to show icons types and allow changes in colors and backgrounds

--- src/demo/comet/demo-icon.ts
+++ src/demo/comet/demo-icon.ts
@@ -1,95 +1,250 @@
 import { css, html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
 import "../../components/icon/comet-icon";
+import {
+  COMPANY_LOGOS_ICONS,
+  FLAGS_ICONS,
+  THREE_DIMENSIONAL_ICONS,
+  UI_ICONS,
+  WECONS_CIRCLE_ICONS,
+  WECONS_HELIX_ICONS,
+} from "../../demo/constants/icons";
+
+const ALL_FILTERS = [
+  "ui",
+  "wecons-helix",
+  "wecons-circle",
+  "three-dimensional",
+  "company-logos",
+  "flags",
+];
+
+// Add this constant to map icon types to their arrays
+const ICON_MAPPING = {
+  ui: { title: "UI icons", icons: UI_ICONS, type: "ui" },
+  "wecons-helix": {
+    title: "Wecons Helix icons",
+    icons: WECONS_HELIX_ICONS,
+    type: "wecons-helix",
+  },
+  "wecons-circle": {
+    title: "Wecons Circle icons",
+    icons: WECONS_CIRCLE_ICONS,
+    type: "wecons-circle",
+  },
+  "three-dimensional": {
+    title: "Three-dimensional icons",
+    icons: THREE_DIMENSIONAL_ICONS,
+    type: "three-dimensional",
+  },
+  "company-logos": {
+    title: "Company logos icons",
+    icons: COMPANY_LOGOS_ICONS,
+    type: "company-logos",
+  },
+  flags: { title: "Flags icons", icons: FLAGS_ICONS, type: "flags" },
+};
+
+const ICON_TYPES_FILTER_ITEMS = Object.entries(ICON_MAPPING).map(
+  ([key, { title }]) => ({
+    value: key,
+    label: title,
+  })
+);
 
 @customElement("demo-icon")
 export class DemoIcon extends LitElement {
+  private _primaryColor: string;
+  private _secondaryColor: string;
+  private _cardBackgroundColor: string;
+  private _selectedFilters: string[] = [];
+
+  constructor() {
+    super();
+
+    this._primaryColor = "#501e96";
+    this._secondaryColor = "#28d7dc";
+    this._cardBackgroundColor = "#ffffff";
+    this._selectedFilters = ALL_FILTERS;
+  }
+
+  public get properties() {
+    return {
+      primaryColor: { type: String },
+      secondaryColor: { type: String },
+      selectedFilters: { type: Array },
+    };
+  }
+
+  get primaryColor() {
+    return this._primaryColor;
+  }
+
+  set primaryColor(color: string) {
+    let oldVal = this._primaryColor;
+    this._primaryColor = color;
+    this.requestUpdate("primaryColor", oldVal);
+  }
+
+  get secondaryColor() {
+    return this._secondaryColor;
+  }
+
+  set secondaryColor(color: string) {
+    let oldVal = this._secondaryColor;
+    this._secondaryColor = color;
+    this.requestUpdate("secondaryColor", oldVal);
+  }
+
+  get selectedFilters() {
+    return this._selectedFilters;
+  }
+
+  set selectedFilters(filters: string[]) {
+    let oldVal = this._selectedFilters;
+    this._selectedFilters = filters;
+    this.requestUpdate("selectedFilters", oldVal);
+  }
+
+  get cardBackgroundColor() {
+    return this._cardBackgroundColor;
+  }
+
+  set cardBackgroundColor(color: string) {
+    let oldVal = this._cardBackgroundColor;
+    this._cardBackgroundColor = color;
+    this.requestUpdate("cardBackgroundColor", oldVal);
+  }
+
+  handlePrimaryColorChange(event: CustomEvent) {
+    this.primaryColor = event.detail.color;
+  }
+
+  handleSecondaryColorChange(event: CustomEvent) {
+    this.secondaryColor = event.detail.color;
+  }
+
+  handleCardBackgroundColorChange(event: CustomEvent) {
+    this.cardBackgroundColor = event.detail.color;
+  }
+
+  handleFilterChange(event: CustomEvent) {
+    const selected = event.detail?.map((item) => item.value);
+
+    if (!selected?.length) {
+      this.selectedFilters = ALL_FILTERS;
+    } else {
+      this.selectedFilters = selected;
+    }
+  }
+
   render() {
     return html`
       <h1 style="color: var(--primary-50); margin-top: 0">Comet Icon</h1>
 
       <div class="icons-container">
-        <h2 style="color: var(--primary-50); margin-top: 0">ui theme</h2>
-        <div id="first-container">
-          <comet-icon
-            id="first-icon-component"
-            name="user"
-            size="100"
-          ></comet-icon>
+        <div>
+          <p>Primary color</p>
+          <comet-color-picker
+            theme="small"
+            .initialColor=${{
+              color_index: 0,
+              variation_index: 4,
+            }}
+            @color-change="${this.handlePrimaryColorChange}"
+          ></comet-color-picker>
         </div>
 
-        <h2 style="color: var(--primary-50); margin-top: 24px">
-          wecons-helix theme
-        </h2>
-        <comet-icon
-          name="globe"
-          primaryColor="success-70"
-          size="100"
-          type="wecons-helix"
-        ></comet-icon>
+        <div>
+          <p>Secondary color</p>
+          <comet-color-picker
+            theme="small"
+            .initialColor=${{
+              color_index: 1,
+              variation_index: 4,
+            }}
+            @color-change="${this.handleSecondaryColorChange}"
+          ></comet-color-picker>
+        </div>
 
-        <h2 style="color: var(--primary-50); margin-top: 24px">
-          wecons-circle theme
-        </h2>
-        <comet-icon
-          name="value-communicate-clearly"
-          size="100"
-          type="wecons-circle"
-        ></comet-icon>
+        <div>
+          <p>Card background color</p>
+          <comet-color-picker
+            theme="small"
+            .initialColor=${{
+              color_index: 7,
+              variation_index: 0,
+            }}
+            @color-change="${this.handleCardBackgroundColorChange}"
+          ></comet-color-picker>
+        </div>
 
-        <h2 style="color: var(--primary-50); margin-top: 24px">
-          three-dimensional theme
-        </h2>
-        <comet-icon
-          type="three-dimensional"
-          name="life"
-          size="100"
-        ></comet-icon>
-
-        <h2 style="color: var(--primary-50); margin-top: 24px">flags theme</h2>
-        <comet-icon
-          name="UnionofSouthAmericannations"
-          size="100"
-          type="flags"
-        ></comet-icon>
-        <comet-icon name="LA" size="100" type="flags"></comet-icon>
-        <comet-icon name="ES" size="100" type="flags"></comet-icon>
-
-        <h2 style="color: var(--primary-50); margin-top: 24px">
-          company-logos theme
-        </h2>
-        <comet-icon
-          name="swiss-life"
-          size="100"
-          type="company-logos"
-        ></comet-icon>
-
-        <h2 style="color: var(--primary-50); margin-top: 24px">URL theme</h2>
-        <comet-icon
-          src="https://cdn-web.wefox.com/public/pkg/@wefox/icons/1.7.11/svg/company-logos/allianz.svg"
-          size="100"
-          type="url"
-        ></comet-icon>
-        <comet-icon
-          src="https://cdn-web.wefox.com/public/pkg/@wefox/icons/1.7.11/svg/three-dimensional/copilot.svg"
-          size="100"
-          type="url"
-        ></comet-icon>
-        <comet-icon
-          src="https://cdn.pixabay.com/photo/2022/08/17/08/57/butterfly-7391954_1280.jpg"
-          size="100"
-          type="url"
-        ></comet-icon>
+        <div style="width: 500px; margin-left: 2rem;">
+          <comet-multi-select
+            style="width: 100%"
+            @select-change="${this.handleFilterChange}"
+            .items="${ICON_TYPES_FILTER_ITEMS}"
+            label="Select Icons to display"
+          ></comet-multi-select>
+        </div>
       </div>
+
+      ${Object.entries(ICON_MAPPING).map(([key, { title, icons, type }]) =>
+        this.selectedFilters.includes(key)
+          ? html`
+              <div style="margin-bottom: 56px;">
+                <h2>${title}</h2>
+                <div
+                  style="display: flex; align-items: center; gap: 12px; width: 100%; flex-wrap: wrap"
+                >
+                  ${icons.map(
+                    (icon) => html`
+                      <div
+                        style="display: flex; flex-direction: column; gap: 12px"
+                      >
+                        <div
+                          style="
+                    background-color: ${this.cardBackgroundColor};
+                    border-radius: 8px;
+                    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+                    padding: 16px;
+                  "
+                        >
+                          <div
+                            style="display: flex; flex-direction: column; align-items: center; gap: 12px"
+                          >
+                            <div style="margin-top: 12px;">
+                              <comet-icon
+                                name="${icon}"
+                                size="48"
+                                type="${type}"
+                                .primaryColor=${this.primaryColor}
+                                .secondaryColor=${this.secondaryColor}
+                              ></comet-icon>
+                            </div>
+                            <span
+                              style="background: white; padding: 4px 8px; border-radius: 4px; margin-top: 8px"
+                              >${icon}</span
+                            >
+                          </div>
+                        </div>
+                      </div>
+                    `
+                  )}
+                </div>
+              </div>
+            `
+          : ""
+      )}
     `;
   }
 
   static styles = css`
     .icons-container {
-      align-items: start;
       display: flex;
-      flex-direction: column;
       padding: var(--spacing-jumbo);
+      gap: 12px;
+      flex-wrap: wrap;
     }
   `;
 }
